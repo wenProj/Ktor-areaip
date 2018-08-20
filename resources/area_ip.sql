ca703adf35b73fb081ec7d9e9ac1d57177835d0b
wdf <wdf163mail@163.com>
2018-08-20 10:10:35 +0800

    添加离线sql

--- resources/area_ip.sql
+++ resources/area_ip.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50722
 Source Host           : 127.0.0.1:3306
-Source Database       : date
+Source Database       : date 
 
 Target Server Type    : MYSQL
 Target Server Version : 50722
